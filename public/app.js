18f721a07d91d19966b8e24ee4c9d5710d474d5a
Jennifer Fahey <jenguin777@gmail.com>
2018-10-07 01:13:36 -0400

    initial files

---
+++ public/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
+
+$('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').focus();
+});
+
+});
