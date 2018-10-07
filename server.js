01d7ad9cdec6b5a254b4d6324b16733441728778
Jennifer Fahey <jenguin777@gmail.com>
2018-10-07 13:36:16 -0400

    undid previous change, renamed Models to models, bestpractice

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-var db = require("./Models");
+var db = require("./models");
 
 var PORT = process.env.PORT || 3001;
 
