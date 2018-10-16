51bd5d626a49b19a914f0869fb67805bf00a3162
Jennifer Fahey <jenguin777@gmail.com>
2018-10-16 11:06:22 -0400

    commenting out delete note, it is broken

--- public/js/app.js
+++ public/js/app.js
@@ -123,26 +123,25 @@ $(document).ready(function() {
 				// $("#note").val("");
 				// $("#title").val("");
 			});
-	});			
-
-	// When user clicks the delete button for a note
-	$(document).on("click", ".delete", function() {
-		// Save the p tag that encloses the button
-		var selected = $(this).parent();
-		// Make an AJAX GET request to delete the specific note
-		// this uses the data-id of the p-tag, which is linked to the specific note
-		$.ajax({
-			type: "PUT",
-			url: "/articles/delete/" + selected.attr("data-id"),
-		
-			// On successful call
-			success: function(response) {
-				// Remove the p-tag from the DOM
-				selected.remove();
-				// Clear the note and title inputs
-				$("#note").val("");
-				$("#title").val("");
-			}
-		});
 	});
+	// When user clicks the delete button for a note
+// 	$(document).on("click", ".delete", function() {
+// 		var thisId = $(this).attr("data-id");
+		
+// 		// Make an AJAX GET request to delete the specific note
+// 		// this uses the data-id of the p-tag, which is linked to the specific note
+// 		$.ajax({
+// 			type: "PUT",
+// 			url: "/articles/delete/" + thisId,
+		
+// 			// On successful call
+// 			success: function(response) {
+// 				// Remove the p-tag from the DOM
+// 				selected.remove();
+// 				// Clear the note and title inputs
+// 				$("#note").val("");
+// 				$("#title").val("");
+// 			}
+// 		});
+// 	});
 });
