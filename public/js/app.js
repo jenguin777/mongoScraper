7fe0f7427a9708066d9cb88d5490269234269556
Jennifer Fahey <jenguin777@gmail.com>
2018-10-16 07:17:24 -0400

    saving notes but always for the first article on the page, regardless of which article the Article Notes button is clicked for

--- public/js/app.js
+++ public/js/app.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
 
 	// When the saveNote button is clicked
 	$("body").on("click", ".save-note", function() {
-		// Grab the id associated with the article from the submit button
+		// Grab the id associated with the article from the Save Note button
 		var thisId = $(this).attr("data-id");
 		console.log("thisId" + thisId);
 
@@ -95,15 +95,18 @@ $(document).ready(function() {
 		})
 			// If that API call succeeds, add the title and a delete button for the note to the page
 			.then(function(data) {
-
-				
+				console.log("data" + JSON.stringify(data));
 				// Add the title and delete button to the #noteArea section
 				// $("#noteArea").prepend("<p class='#noteArea' data-id=" + data._id + "><span class='dataTitle' data-id=" +
 				// 	data._id + ">" + data.title + "</span><span class=delete>X</span></p>");
 				
-				console.log(data);
+				$("#noteArea").attr("data-id",thisId);
+				
 				// The title of the article
 				$("#note-header").append("<h6>" + data.title + "</h6>");
+
+
+				// Another approach --------------------------------
 				// // An input to enter a new title
 				// $("#noteArea").append("<input id='noteTitleInput' name='title' >");
 				// // A textarea to add a new note body
@@ -111,44 +114,40 @@ $(document).ready(function() {
 				// // A button to submit a new note, with the id of the article saved to it
 				// $("#noteArea").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 		  
-				// // If there's a note in the article
-				// if (data.note) {
-				//   // Place the title of the note in the title input
-				//   $("#titleinput").val(data.note.title);
-				//   // Place the body of the note in the body textarea
-				//   $("#bodyinput").val(data.note.body);
-				// }
+				// If there's a note in the article
+				if (data.note) {
+				  // Place the title of the note in the title input
+				  $("noteTitleInput").val(data.note.title);
+				  // Place the body of the note in the body textarea
+				  $("noteBodyInput").val(data.note.body);
+				}
 
-				
-				
 					// Clear the note and title inputs on the page
 				// $("#note").val("");
 				// $("#title").val("");
 			});
 	});			
-});
-  
 
-// When user clicks the delete button for a note
-$(document).on("click", ".delete", function() {
-	// Save the p tag that encloses the button
-	var selected = $(this).parent();
-	// Make an AJAX GET request to delete the specific note
-	// this uses the data-id of the p-tag, which is linked to the specific note
-	$.ajax({
-		type: "GET",
-		url: "/delete/" + selected.attr("data-id"),
-	
-		// On successful call
-		success: function(response) {
-			// Remove the p-tag from the DOM
-			selected.remove();
-			// Clear the note and title inputs
-			$("#note").val("");
-			$("#title").val("");
-			// Make sure the #action-button is submit (in case it's update)
-			$("#action-button").html("<button id='make-new'>Submit</button>");
-		}
+	// When user clicks the delete button for a note
+	$(document).on("click", ".delete", function() {
+		// Save the p tag that encloses the button
+		var selected = $(this).parent();
+		// Make an AJAX GET request to delete the specific note
+		// this uses the data-id of the p-tag, which is linked to the specific note
+		$.ajax({
+			type: "GET",
+			url: "/delete/" + selected.attr("data-id"),
+		
+			// On successful call
+			success: function(response) {
+				// Remove the p-tag from the DOM
+				selected.remove();
+				// Clear the note and title inputs
+				$("#note").val("");
+				$("#title").val("");
+				// Make sure the #action-button is submit (in case it's update)
+				$("#action-button").html("<button id='make-new'>Submit</button>");
+			}
+		});
 	});
 });
-  
--- routes/article.js
+++ routes/article.js
@@ -73,6 +73,7 @@ router.post("/save-note/:id", function(req, res) {
 	// and update it's "note" property with the _id of the new note
 	db.Note.create(req.body)
 		.then(function(dbNote) {
+
 			return db.Article.findOneAndUpdate({
 				_id: req.params.id
 			}, {
@@ -82,7 +83,9 @@ router.post("/save-note/:id", function(req, res) {
 				new: true
 			}).populate("note");
 		})
+		// If the Article was updated successfully, send it back to the client
 		.then(function(dbArticle){
+			//Need to figure out how to change this so that it sends back botht he article and the notes
 			res.json(dbArticle);
 		})
 		.catch(function(err) {
